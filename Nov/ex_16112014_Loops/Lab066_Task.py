d629f312ea31070462d36e53861a085c03946cfe
pratik150696 <alaspurepratik0@gmail.com>
2024-11-17 01:53:05 +0530

    16 Nov Task added

--- Nov/ex_16112014_Loops/Lab066_Task.py
+++ Nov/ex_16112014_Loops/Lab066_Task.py
@@ -4,7 +4,7 @@
 # For numbers that are multiples of both 3 and 5, print "FizzBuzz." ( for, if)
 
 
-for number in range(1, 100):
+for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
     elif number % 3 == 0:
